19069c8cf633adf8b54043bbad8867b8ad26bd5f
Pepeelche1 <pepeelche4@gmail.com>
2023-04-02 20:38:12 +0200

    refactorizar metodos

--- tests/TestCoche.java
+++ tests/TestCoche.java
@@ -3,10 +3,10 @@ import org.junit.jupiter.api.Test;
 
 public class TestCoche {
     @Test
-    public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero(){
+    public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero_JoseDanielGuijarro(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
-        nuevoCoche.decelerar(80);
+        nuevoCoche.decelerar_JoseDanielGuijarro(80);
         Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
 }
